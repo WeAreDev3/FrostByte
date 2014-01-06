fda65c781c846223ae9b58ca0a517407790c456e
Jesse <janka102@gmail.com>
2014-01-05 17:44:23 -0800

    Improved how the characters and guns are created
    Also changed some gun specs,
    improved the relationship between Guns and Bullets,
        - Now each bullet has a gun and each gun has a character
    and moved bullet updating into its own function.
    Just for kicks, also added an option to draw the characters as circles

--- main.js
+++ main.js
@@ -23,13 +23,35 @@ window.onload = function () {
         'mouseDown': false
     };
 
-    var semiAuto = new Gun('semi-auto', 0.05, 15, 5, 30, 75),
-        fullAuto = new Gun('full-auto', 0.07, 10, 5, 25, 100),
-        shotgun = new Gun('shotgun', 0.3, 7, 7, 18, 45);
-
-    player = new Character('player', 'Bob', 12, 100, 10, [canvas.width / 2, canvas.height / 2], Math.PI / 2, fullAuto);
     crosshairs = new Crosshairs(canvas.width / 2 + 100, canvas.height / 2 + 100);
+    /*
+      (In order)
+      type: Name of gun
+      accuracy: Higher is WORSE
+      damage: Higher is BETTER
+      kick: Higher is WORSE
+      bulletSpeed: Higher is BETTER
+      rate: Higher is WORSE
+    */
 
+    var semiAuto = ['semi-auto', 5, 30, 5, 17, 140],
+        fullAuto = ['full-auto', 8, 20, 5, 10, 100],
+        shotgun = ['shotgun', 13, 10, 7, 17, 220],
+        playerSpecs = {
+            'type': 'player',
+            'name': 'Bob',
+            'size': 12,
+            'speed': 100,
+            'mobility': 10,
+            'x': canvas.width / 2,
+            'y': canvas.height / 2,
+            'direction': Math.PI / 2,
+            'gun': create(Gun, fullAuto),
+            'color': '#4D90FE'
+        },
+    player = new Character(playerSpecs);
+
+    console.log(player);
     console.log(crosshairs);
 
     //We use a loop to keep the entire program synchronous
@@ -63,6 +85,14 @@ window.onload = function () {
 function initCanvas () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+}
+
+function create(constructor, args) {
+    function F() {
+        return constructor.apply(this, args);
+    }
+    F.prototype = constructor.prototype;
+    return new F();
 }
 
 bullets = [];
@@ -131,16 +161,18 @@ Crosshairs.prototype.update = function (timeElapsed) {
     this.y = player.y + this.distFromPlayer * Math.sin(this.angle);
 };
 
-Character = function (type, name, size, speed, mobility, position, direction, gun) {
-    this.type = type;
-    this.name = name;
-    this.size = size;
-    this.speed = speed;
-    this.mobility = mobility;
-    this.x = position[0];
-    this.y = position[1];
-    this.direction = direction;
-    this.gun = gun;
+Character = function(specs) {
+    this.type = specs.type;
+    this.name = specs.name;
+    this.size = specs.size;
+    this.speed = specs.speed;
+    this.mobility = specs.mobility;
+    this.x = specs.x;
+    this.y = specs.y;
+    this.direction = specs.direction;
+    this.gun = specs.gun;
+    this.gun.character = this;
+    this.color = specs.color;
 };
 
 Character.prototype.draw = function () {
@@ -148,12 +180,20 @@ Character.prototype.draw = function () {
         namePositionY = this.y + (this.size * 2);
 
     // Draws Body
+    context.fillStyle = this.color;
+
+    // Square
     context.save();
     context.translate(this.x, this.y);
     context.rotate(this.direction);
-    context.fillStyle = '#4D90FE';
     context.fillRect(-1 * this.size, -1 * this.size, this.size * 2, this.size * 2);
     context.restore();
+
+    // Circle
+    // context.beginPath();
+    // context.arc(this.x, this.y, 15, 0, 2 * Math.PI, false);
+    // context.fill();
+    // context.closePath();
 
     // Draws Name
     context.fillStyle = '#000';
@@ -178,7 +218,7 @@ Character.prototype.update = function (timeElapsed) {
         this.direction = Math.atan2((crosshairs.y - this.y), (crosshairs.x - this.x)) + Math.PI;
 
         if (input.mouseDown) {
-            this.gun.fire(this);
+            this.gun.fire();
         }
 
         this.gun.timeSinceLastFire += timeElapsed;
@@ -196,11 +236,11 @@ Character.prototype.update = function (timeElapsed) {
     }
 };
 
-Bullet = function(character, speed, direction) {
-    this.character = character;
+Bullet = function(gun, speed, direction) {
+    this.gun = gun;
     this.speed = speed;
-    this.x = character.x;
-    this.y = character.y;
+    this.x = this.gun.character.x;
+    this.y = this.gun.character.y;
     this.direction = direction;
 
     bullets.push(this);
@@ -217,6 +257,17 @@ Bullet.prototype.draw = function() {
     context.lineWidth = 1;
     context.strokeStyle = '#000';
     context.stroke();
+    context.closePath();
+};
+
+Bullet.prototype.update = function(timeElapsed) {
+    if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
+        bullets.splice(bullets.indexOf(this), 1);
+        return;
+    }
+
+    this.x -= this.speed * Math.cos(this.direction) * timeElapsed * 100;
+    this.y -= this.speed * Math.sin(this.direction) * timeElapsed * 100;
 };
 
 Gun = function(type, accuracy, damage, kick, bulletSpeed, rate) {
@@ -228,9 +279,10 @@ Gun = function(type, accuracy, damage, kick, bulletSpeed, rate) {
     this.wasFired = false;
     this.rate = rate / 1000;
     this.timeSinceLastFire = 0;
+    this.character = null;
 };
 
-Gun.prototype.fire = function(character) {
+Gun.prototype.fire = function() {
     if (this.timeSinceLastFire >= this.rate) {
         this.timeSinceLastFire -= this.rate;
 
@@ -238,19 +290,19 @@ Gun.prototype.fire = function(character) {
             case 'semi-auto':
                 if (!this.wasFired) {
                     this.wasFired = true;
-                    new Bullet(character, this.bulletSpeed, character.direction);
+                    new Bullet(this, this.bulletSpeed, this.character.direction);
                     crosshairs.kickCounter++;
                 }
                 break;
             case 'full-auto':
-                new Bullet(character, this.bulletSpeed, character.direction);
+                new Bullet(this, this.bulletSpeed, this.character.direction);
                 crosshairs.kickCounter++;
                 break;
             case 'shotgun':
                 if (!this.wasFired) {
                     this.wasFired = true;
-                    for (var i = 0; i < 4; i++) {
-                        new Bullet(character, this.bulletSpeed, character.direction + (Math.random() * 2 - 1) * player.gun.kick / 100);
+                    for (var i = 0; i < 5; i++) {
+                        new Bullet(this, this.bulletSpeed, this.character.direction + (Math.random() * 2 - 1) * this.character.gun.accuracy / 100);
                     }
                     crosshairs.kickCounter++;
                 }
@@ -264,11 +316,18 @@ function update (timeElapsed) {
     /*Every frame, update will run commands and call functions to update
         the necessary game variables so that the draw function can draw them
         out properly.*/
+    // Can't cache the length of the arrays b/c they can change mid-loop.
+    for (i = 0; i < bullets.length; i++) {
+        bullets[i].update(timeElapsed);
+    }
+
     player.update(timeElapsed);
     crosshairs.update(timeElapsed);
 }
 
 function draw (context) {
+    var i,
+        len;
     /*Every frame, draw will clear the frame and redraw all
       of the onscreen elements with the updated variables.*/
 
@@ -281,8 +340,6 @@ function draw (context) {
 
     for (var i = 0, len = bullets.length; i < len; i++) {
         bullets[i].draw();
-        bullets[i].x -= bullets[i].speed * Math.cos(bullets[i].direction);
-        bullets[i].y -= bullets[i].speed * Math.sin(bullets[i].direction);
     }
 }
 
