d21e9f74ddc590a25d87dd6dbf94f99586fc1180
Jesse <janka102@gmail.com>
2014-01-07 22:35:52 -0800

    Added some comments and updated some gun/player attributes

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ window.onload = function() {
     canvas = document.getElementById('frame');
     context = canvas.getContext('2d');
 
-    initCanvas();
+    initCanvas(); //Sets up the canvas so that it looks right on the screen
 
+    //Event handlers, pretty straightforward stuff
     window.onkeydown = handleKeyDown;
     window.onkeyup = handleKeyUp;
     window.onmousemove = handleMouseMove;
     window.onmousedown = handleMouseDown;
     window.onmouseup = handleMouseUp;
-    window.onresize = initCanvas;
+    window.onresize = initCanvas; //Re-set-up the canvas every time the browser is resized
 
+    //Some environment input variables, updated by the event handlers from above
     input = {
         'w': false,
         'a': false,
@@ -34,7 +36,7 @@ window.onload = function() {
     */
 
     var semiAuto = ['semi-auto', 5, 30, 5, 17, 140],
-        fullAuto = ['full-auto', 8, 20, 5, 10, 100],
+        fullAuto = ['full-auto', 8, 20, 5, 15, 100],
         shotgun = ['shotgun', 13, 10, 7, 17, 220],
         playerSpecs = {
             'type': 'player',
@@ -54,7 +56,7 @@ window.onload = function() {
             'name': null,
             'hp': 50,
             'size': 12,
-            'speed': 45,
+            'speed': 55,
             'mobility': 10,
             'x': null,
             'y': null,
@@ -63,9 +65,13 @@ window.onload = function() {
             'color': 'rbg(0,255,0)'
         };
 
+    //Create the player!
     player = new Character(playerSpecs);
-    crosshairs = new Crosshairs(canvas.width / 2, canvas.height / 2);
 
+    //Create the crosshairs!
+    crosshairs = new Crosshairs(canvas.width / 2, canvas.height / 2, player);
+
+    //Create the enemies!
     for (var i = 0; i < 3; i++) {
         enemySpecs.name = i;
         enemySpecs.x = Math.random() * canvas.width;
@@ -78,10 +84,9 @@ window.onload = function() {
 
     console.log(player);
     console.log(crosshairs);
-    console.log(enemies);
+    console.log('Enemies:', enemies);
 
     //We use a loop to keep the entire program synchronous
-
     function startLoop() {
         var frameId = 0,
             lastFrame = Date.now(),
@@ -109,6 +114,7 @@ window.onload = function() {
     startLoop();
 };
 
+//Expands the canvas to the full width and height of the browser window
 function initCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -122,14 +128,16 @@ function create(constructor, args) {
     return new F();
 }
 
+//Initalize the arrays of bullets and enemies
 bullets = [];
 enemies = [];
 
+/*Every frame, update will run commands and call functions to update
+  the necessary game variables so that the draw function can draw them
+  out properly.*/
 function update(timeElapsed) {
     var i;
-    /*Every frame, update will run commands and call functions to update
-        the necessary game variables so that the draw function can draw them
-        out properly.*/
+    
 
     // Can't cache the length of the arrays b/c they can change mid-loop.
     for (i = 0; i < enemies.length; i++) {
@@ -144,11 +152,12 @@ function update(timeElapsed) {
     crosshairs.update(timeElapsed);
 }
 
+
+/*Every frame, draw will clear the frame and redraw all
+  of the onscreen elements with the updated variables.*/
 function draw(context) {
     var i,
         len;
-    /*Every frame, draw will clear the frame and redraw all
-      of the onscreen elements with the updated variables.*/
 
     //Step 1: Clear the screen
     clearScreen();
@@ -164,10 +173,11 @@ function draw(context) {
         bullets[i].draw();
     }
 
-    //Step 2a: Crosshairs should be last so it is always on top.
+    //Crosshairs should be drawn last so it is always on top
     crosshairs.draw();
 }
 
+//Record the needed keys in the input object
 function handleKeyDown(event) {
     switch (event.keyCode) {
         case 87: // w
@@ -185,6 +195,7 @@ function handleKeyDown(event) {
     }
 }
 
+//Remove the keys recorded from the input object
 function handleKeyUp(event) {
     switch (event.keyCode) {
         case 87: // w
@@ -202,6 +213,7 @@ function handleKeyUp(event) {
     }
 }
 
+//Record the location of the cursor in the input object
 function handleMouseMove(event) {
     input.mouse.x = event.x;
     input.mouse.y = event.y;
@@ -210,10 +222,12 @@ function handleMouseMove(event) {
     cursor.style.top = event.y - cursor.height / 2 + 'px';
 }
 
+//Record a mouse button press in the input object
 function handleMouseDown() {
     input.mouseDown = true;
 }
 
+//Record the    
 function handleMouseUp() {
     input.mouseDown = false;
 }
