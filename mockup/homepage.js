b00dedb70aed7d0c8393273bfa032a9bb2341f9d
Jesse <janka102@gmail.com>
2014-01-10 13:54:26 -0800

    Prettified

--- mockup/homepage.js
+++ mockup/homepage.js
@@ -1,23 +1,23 @@
-window.onload = function () {
-	var playButton = document.getElementById('play');
-	playButton.onclick = setupGame;
-}
+window.onload = function() {
+    var playButton = document.getElementById('play');
+    playButton.onclick = setupGame;
+};
 
-function setupGame () {
-	var editItems = [document.getElementById('play'), document.getElementById('follow'), document.getElementById('info')];
+function setupGame() {
+    var editItems = [document.getElementById('play'), document.getElementById('follow'), document.getElementById('info')];
 
-	//Add an opacity fade to each item that needs it
-	for (var i = editItems.length - 1; i >= 0; i--) {
-		editItems[i].classList.add('opacity-transition');
-	};
+    //Add an opacity fade to each item that needs it
+    for (var i = editItems.length - 1; i >= 0; i--) {
+        editItems[i].classList.add('opacity-transition');
+    }
 
-	window.setTimeout(function () {
-		//Remove the items from the screen completely
-		for (var i = editItems.length - 1; i >= 0; i--) {
-			editItems[i].classList.add('remove-display');
-		};
-	}, 250);
+    window.setTimeout(function() {
+        //Remove the items from the screen completely
+        for (var i = editItems.length - 1; i >= 0; i--) {
+            editItems[i].classList.add('remove-display');
+        }
+    }, 250);
 
-	//Notify elements of stage 2 of game setup
-	document.getElementsByTagName('body')[0].classList.add('stage2');
+    //Notify elements of stage 2 of game setup
+    document.getElementsByTagName('body')[0].classList.add('stage2');
 }
--- server.js
+++ server.js
@@ -22,17 +22,17 @@ var io = socketio.listen(server);
 router.use(express.static(path.resolve(__dirname, 'client')));
 var sockets = [];
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
     console.log('Client connected', socket);
     sockets.push(socket);
 
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      console.log('Client disconnected', socket);
+    socket.on('disconnect', function() {
+        sockets.splice(sockets.indexOf(socket), 1);
+        console.log('Client disconnected', socket);
     });
 });
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Server listening at", addr.address + ":" + addr.port);
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+    var addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port);
 });
