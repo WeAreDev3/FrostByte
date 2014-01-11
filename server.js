637528e2aa7663bfffe2b4a6678846738459e536
Jesse <janka102@gmail.com>
2014-01-10 17:42:34 -0800

    Improved socket io configuration

--- server.js
+++ server.js
@@ -22,13 +22,26 @@ var io = socketio.listen(server);
 router.use(express.static(path.resolve(__dirname, 'client')));
 var sockets = [];
 
+io.configure(function() {
+
+    io.set('log level', 0);
+
+    io.set('authorization', function(handshakeData, callback) {
+        callback(null, true); // error first callback style
+    });
+
+});
+
 io.on('connection', function(socket) {
-    console.log('Client connected', socket);
+    console.log('Client connected:', socket.id);
+    socket.emit('onconnected', {
+        id: socket.id
+    });
     sockets.push(socket);
 
     socket.on('disconnect', function() {
         sockets.splice(sockets.indexOf(socket), 1);
-        console.log('Client disconnected', socket);
+        console.log('Client disconnected:', socket.id);
     });
 });
 
