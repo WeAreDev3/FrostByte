dbd1a9b25954e5c016e1696600c27d84de3ea723
Jesse <janka102@gmail.com>
2014-01-10 17:46:49 -0800

    PERFECTED bullet hit detection!

--- client/js/bullet.js
+++ client/js/bullet.js
@@ -2,18 +2,20 @@ Bullet = function(gun, speed, direction) {
     this.gun = gun;
     this.speed = speed;
     this.direction = direction;
-    this.x = this.gun.character.x - (5 + this.gun.character.size) * Math.cos(this.direction);
-    this.y = this.gun.character.y - (5 + this.gun.character.size) * Math.sin(this.direction);
+    this.x1 = this.gun.character.x - (5 + this.gun.character.size) * Math.cos(this.direction);
+    this.y1 = this.gun.character.y - (5 + this.gun.character.size) * Math.sin(this.direction);
+    this.x2 = this.x1 - 10 * Math.cos(this.direction);
+    this.y2 = this.y1 - 10 * Math.sin(this.direction);
+    this.prevX = this.x1;
 
     bullets.push(this);
 };
 
 Bullet.prototype.draw = function() {
     context.beginPath();
-    context.moveTo(this.x, this.y);
+    context.moveTo(this.x1, this.y1);
 
-    context.lineTo(this.x - (1.5 * this.gun.character.size) * Math.cos(this.direction),
-        this.y - (1.5 * this.gun.character.size) * Math.sin(this.direction));
+    context.lineTo(this.x2, this.y2);
 
     context.lineWidth = 1;
     context.strokeStyle = '#000';
@@ -22,14 +24,30 @@ Bullet.prototype.draw = function() {
 };
 
 Bullet.prototype.update = function(timeElapsed) {
-    if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
+    if (this.x1 < 0 || this.x1 > canvas.width || this.y1 < 0 || this.y1 > canvas.height) {
         bullets.splice(bullets.indexOf(this), 1);
         return;
     }
 
+    var m = (this.y2 - this.y1) / (this.x2 - this.x1),
+        m2 = -1 / m,
+        intersection = [];
+
     for (var i = 0, len = enemies.length; i < len; i++) {
-        if (Math.sqrt(Math.pow((enemies[i].x - this.x), 2) + Math.pow((enemies[i].y - this.y), 2)) <= enemies[i].size) {
+        // Notes...
+        intersection[0] = ((enemies[i].x * -m2) + (this.x1 * m) + enemies[i].y - this.y1) / (m - m2);
+        intersection[1] = m * intersection[0] - (m * this.x1) + this.y1;
+
+        var distanceFromBulletLine = Math.sqrt(Math.pow((enemies[i].x - intersection[0]), 2) + Math.pow((enemies[i].y - intersection[1]), 2)),
+            isIntersectionOnBullet = this.x2 > this.prevX ? intersection[0] > this.prevX && intersection[0] < this.x2 : intersection[0] < this.prevX && intersection[0] > this.x2;
+
+        if (distanceFromBulletLine <= enemies[i].size && isIntersectionOnBullet) {
+            debugger;
+
+            // Hit
             enemies[i].health -= this.gun.damage;
+
+            // Kill
             if (enemies[i].health <= 0) {
                 enemies[i].health = 0;
                 enemies[i].speed = 0;
@@ -41,6 +59,9 @@ Bullet.prototype.update = function(timeElapsed) {
         }
     }
 
-    this.x -= this.speed * Math.cos(this.direction) * timeElapsed * 100;
-    this.y -= this.speed * Math.sin(this.direction) * timeElapsed * 100;
+    this.prevX = this.x1;
+    this.x1 -= this.speed * Math.cos(this.direction) * timeElapsed * 100;
+    this.y1 -= this.speed * Math.sin(this.direction) * timeElapsed * 100;
+    this.x2 = this.x1 - 10 * Math.cos(this.direction);
+    this.y2 = this.y1 - 10 * Math.sin(this.direction);
 };
