61ba4d01e4d3780412e88e7a20213a8f65ea6557
Tal Ben-Ari <talbenari1@gmail.com>
2014-01-11 23:39:41 -0800

    Started work on server game logic.

--- server/game.js
+++ server/game.js
@@ -1,1 +1,42 @@
-//Stuff
+var Game = function() {
+    this.width = 1600;
+    this.height = 1000;
+
+    startLoop();
+};
+
+Game.prototype.addChar = function(client) {
+	chars.push(new Character);
+};
+
+chars = [];
+
+function startLoop() {
+    var lastFrame = 0,
+        count = 0;
+
+    window.setInterval(function() {
+        var thisFrame = Date.now(),
+            timeElapsed = (thisFrame - lastFrame) / 1000;
+
+        physUpdate(timeElapsed);
+
+        if (count % 3 == 0) {
+            serveUpdate(timeElapsed);
+        };
+
+        lastFrame = thisFrame;
+        count++;
+    }, 15);
+}
+
+function physUpdate(timeElapsed) {
+    //Update the physics
+
+}
+
+function serveUpdate(timeElapsed) {
+    //Serve the update to the clients
+}
+
+module.exports = Game;
