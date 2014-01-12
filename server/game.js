052d4e3f356cf713c50aa58ab057428fe16d6bff
Jesse <janka102@gmail.com>
2014-01-12 14:45:48 -0800

    Comments by Tal. And added a way to delete players from game

--- server/game.js
+++ server/game.js
@@ -1,57 +1,78 @@
+// Link our files together
 var Character = require('./character');
 
+// The Game class's definition
 var Game = function() {
-    this.width = 1600;
-    this.height = 1000;
+    // Define the width and height of the server's viewport -
+    // we will be using a 16:10 aspect ratio
+    this.width = 1600; // = 16 * 100
+    this.height = 1000; // = 10 * 100
+
+    // An object containing all of the players in the game
     this.players = {};
 
+    // Start the game loop
     this.startLoop();
 };
 
+// Add the client's character to the game
 Game.prototype.addChar = function(client) {
     this.players[client.id] = new Character(client);
 };
 
+// Remove the client's character from the game
+Game.prototype.removeChar = function(client) {
+    // console.log('Deleting:', this.players[client.id]);
+    delete this.players[client.id];
+};
+
+// Start the synchronized game loop
 Game.prototype.startLoop = function() {
-    var lastFrame = 0,
-        count = 0,
-        self = this;
+    var lastFrame = 0, // Initialize the game loop
+        count = 0, // Initialize the interval counter
+        self = this; // Capture the game object for usage below
 
+    // Update the physics of the game
     function physUpdate(timeElapsed) {
-        //Update the physics
-
     }
 
+    // Serve the updated game to the clients
     function serveUpdate(timeElapsed) {
-        //Serve the update to the clients
+        // The object containing the information the clients use to update
         var update = {};
 
-
+        // For each player,
         for (var player in self.players) {
             if (self.players.hasOwnProperty(player)) {
+                // Add their array to the update object
                 update[player] = [];
+                // Add their x and y position to their array
                 update[player].push(self.players[player].x);
                 update[player].push(self.players[player].y);
             }
         }
-
+        
+        // Send the data to each client
         for (var player in self.players) {
             if (self.players.hasOwnProperty(player)) {
-                self.players[player].client.emit('update', {
-                    'u': update
-                });
+                self.players[player].client.emit('update', update);
             }
         }
     }
 
+    // Set the update interval to 15ms (see end of call)
     setInterval(function() {
+        // Define the time elapsed since the last frame
         var thisFrame = Date.now(),
             timeElapsed = (thisFrame - lastFrame) / 1000;
 
+        // Update the physics of the game
         physUpdate(timeElapsed);
 
+        // Every 3 intervals (45ms),
         if (count % 3 == 0) {
             count = 0;
+            // Serve the update to the clients
             serveUpdate(timeElapsed);
         };
 
