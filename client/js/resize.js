f285deffdae0026ffe93eb466a54b601428e0539
Tal Ben-Ari <talbenari1@gmail.com>
2014-01-14 19:53:27 -0800

    Removed console logs from resize.js.

--- client/js/resize.js
+++ client/js/resize.js
@@ -3,16 +3,12 @@ function resizeBrowser() {
     var width = window.innerWidth,
         height = window.innerHeight;
 
-    console.log('The page is', width, 'x', height);
 
     if (width / height >= 1.6) {
         width = height * 1.6;
     } else {
         height = width / 1.6;
     }
-    console.log('The canvas is', width, 'x', height);
 
     scale = height / 1000;
-
-    console.log('The page has been scaled by a factor of', scale);
 }
