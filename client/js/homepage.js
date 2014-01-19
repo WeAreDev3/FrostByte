a9cc17a09cd485d59c2972006ca6180a711a6c40
Jesse Smick <janka102@gmail.com>
2014-01-18 18:32:04 -0800

    Commented

--- client/js/homepage.js
+++ client/js/homepage.js
@@ -7,5 +7,7 @@ function setupGame() {
     document.getElementById('intro').classList.add('playing');
     document.getElementById('frame').classList.add('playing');
     document.getElementsByTagName('html')[0].classList.add('playing');
+
+    // Tell the  server we are ready to play the game (damn it, I lost the game)
     socket.emit('play');
 }
