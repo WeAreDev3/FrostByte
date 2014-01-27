0d7b17861bc21d795dcb5364ad870fdfd80f9cad
Jesse Smick <janka102@gmail.com>
2014-01-27 11:49:21 -0800

    Now enemies spawn every half second until the are all spawned

--- server/game.js
+++ server/game.js
@@ -13,7 +13,8 @@ var Game = Class.extend({
         this.enemies = {};
         this.bullets = {};
 
-        this.level = 0;
+        this.level = 1;
+        this.spawningEnemies = 0;
         this.nextLevel();
 
         this.start();
@@ -28,49 +29,53 @@ var Game = Class.extend({
         this.enemies[enemy.id] = enemy;
     },
     nextLevel: function() {
+        console.log('\nLevel:', this.level);
+
         this.forEachPlayer(function(player, id) {
             player.resetHitPoints();
             console.log(player.name, 'has done', Utils.formatNumber(player.stats.damage), 'damage.');
             console.log(player.name, 'has killed', Utils.formatNumber(player.stats.kills), player.stats.kills !== 1 ? 'enemies.' : 'enemy.');
         });
-        this.spawnEnemies(16 * ((this.level + 1) / 2), this.level);
+
+        this.spawningEnemies = 8 * this.level;
+        this.spawnTime = new Date().getTime();
         this.level++;
     },
     removeEnemy: function(enemy) {
         delete this.enemies[enemy.id];
     },
-    spawnEnemies: function(number) {
-        // console.log('spawing', number);
+    spawnEnemy: function() {
         var location = {
             'x': 0,
             'y': 0
         };
 
-        for (var i = 0; i < number; i++) {
-            switch (Utils.randomInt(4)) {
-                case 0: // Start at top
-                    location.x = Utils.randomInt(this.width);
-                    location.y = 0;
-                    break;
+        switch (Utils.randomInt(4)) {
+            case 0: // Start at top
+                location.x = Utils.randomInt(this.width);
+                location.y = 0;
+                break;
 
-                case 1: // Start from the right
-                    location.x = this.width;
-                    location.y = Utils.randomInt(this.height);
-                    break;
+            case 1: // Start from the right
+                location.x = this.width;
+                location.y = Utils.randomInt(this.height);
+                break;
 
-                case 2: // Start at the bottom
-                    location.x = Utils.randomInt(this.width);
-                    location.y = this.height;
-                    break;
+            case 2: // Start at the bottom
+                location.x = Utils.randomInt(this.width);
+                location.y = this.height;
+                break;
 
-                case 3: // Start from the left
-                    location.x = 0;
-                    location.y = Utils.randomInt(this.height);
-                    break;
-            }
-
-            this.addEnemy(new Enemy(location.x, location.y, this.level, this));
+            case 3: // Start from the left
+                location.x = 0;
+                location.y = Utils.randomInt(this.height);
+                break;
         }
+
+        this.addEnemy(new Enemy(location.x, location.y, this.level, this));
+        this.spawningEnemies--;
+
+        this.spawnTime = new Date().getTime();
     },
     forEachPlayer: function(callback) {
         for (var playerID in this.players) {
@@ -113,8 +118,14 @@ var Game = Class.extend({
                 bullet.update(timeElapsed);
             });
 
-            if (!Object.keys(self.enemies).length) {
-                self.nextLevel();
+            if (self.spawningEnemies > 0) {
+                if (new Date().getTime() - self.spawnTime > 500) {
+                    self.spawnEnemy();
+                }
+            } else {
+                if (!Object.keys(self.enemies).length) {
+                    self.nextLevel();
+                }
             }
         }
 
