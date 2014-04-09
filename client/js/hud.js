89201505b165c43b9ac5527c840bf83edb3d5591
Jesse Smick <janka102@gmail.com>
2014-04-09 01:40:43 -0700

    Got the numbers to change on setting health

--- client/js/hud.js
+++ client/js/hud.js
@@ -2,6 +2,8 @@ Hud = Class.extend({
     init: function(hud) {
 
         this.hud = hud;
+        this.playerHealth = document.getElementById('player-health').childNodes[0];
+        this.maxPlayerHealth = document.getElementById('max-player-health');
         this.healthMeter = document.getElementById('health-meter');
         this.score = document.getElementById('player-score');
         this.kills = document.getElementById('player-kills');
@@ -10,9 +12,11 @@ Hud = Class.extend({
         if (maxHitPoints !== undefined) {
             this.healthMeter.max = maxHitPoints;
             this.healthMeter.low = maxHitPoints * 0.3;
+            this.maxPlayerHealth.textContent = maxHitPoints;
         }
 
         this.healthMeter.value = hitPoints;
+        this.playerHealth.nodeValue = hitPoints;
     },
     setScore: function(score) {
         this.score.textContent = score;
