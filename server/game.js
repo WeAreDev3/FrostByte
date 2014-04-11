868a2cef9a0ea4cdbb1c3adf47c27eef0c56ffd3
Tal <talbenari1@gmail.com>
2014-04-10 22:12:19 -0700

    Commented server-side game.js.

--- server/game.js
+++ server/game.js
@@ -1,36 +1,44 @@
-// Link our files together
-var Class = require('./class'),
-    Bullet = require('./bullet'),
-    Enemy = require('./enemy'),
-    Utils = require('./utils');
+var Class = require('./class'), // John Resig's Class Inheritance
+    Bullet = require('./bullet'), // The Bullet class
+    Enemy = require('./enemy'), // The Enemy class
+    Utils = require('./utils'); // Utils, to keep code DRY
 
 var Game = Class.extend({
     init: function() {
+        // Set the game's aspect ratio to 16:10
         this.width = 1600; // = 16 * 100
         this.height = 1000; // = 10 * 100
 
+        // Initialize all of the necessary objects
         this.players = {};
         this.enemies = {};
         this.bullets = {};
 
+        // Define any needed variables
         this.level = 1;
         this.spawningEnemies = 0;
         this.nextLevel();
 
+        // Let the games... begin!
         this.start();
     },
+    // Add a new bullet to the game
     addBullet: function(bullet) {
         this.bullets[bullet.id] = bullet;
     },
+    // Remove the specified bullet from the game
     removeBullet: function(bullet) {
         delete this.bullets[bullet.id];
     },
+    // Add a new enemy to the game
     addEnemy: function(enemy) {
         this.enemies[enemy.id] = enemy;
     },
+    // Increment the level and update the players
     nextLevel: function() {
         console.log('\nLevel:', this.level);
 
+        // Reset the round score and the health for each player
         this.forEachPlayer(function(player, id) {
             player.resetHitPoints();
             player.resetRoundScore();
@@ -40,21 +48,28 @@ var Game = Class.extend({
             console.log(player.name, 'has ', Utils.formatNumber(player.stats.score), 'points.');
         });
 
+        // Make more enemies this time
         this.spawningEnemies = 8 * this.level;
+        // Keep a record of the spawn time
         this.spawnTime = Date.now();
+        // Increment the level variable (finally)
         this.level++;
     },
+    // Remove the specified enemy from the game
     removeEnemy: function(enemy) {
         delete this.enemies[enemy.id];
     },
+    // Spawn a fresh enemy
     spawnEnemy: function() {
+        // Initialize the location variables
         var location = {
             'x': 0,
             'y': 0
         };
 
+        // Pick a random side of the screen to invade from
         switch (Utils.randomInt(4)) {
-            case 0: // Start at top
+            case 0: // Start from the top
                 location.x = Utils.randomInt(this.width);
                 location.y = 0;
                 break;
@@ -75,11 +90,15 @@ var Game = Class.extend({
                 break;
         }
 
+        // Create the new enemy object and simultaneously add it to the object of enemies
         this.addEnemy(new Enemy(location.x, location.y, this.level, this));
+        // Decrement the number of remaining enemies to spawn
         this.spawningEnemies--;
 
+        // Record the latest spawn time
         this.spawnTime = Date.now();
     },
+    // Allows callback to be called on all players at once
     forEachPlayer: function(callback) {
         for (var playerID in this.players) {
             if (this.players.hasOwnProperty(playerID)) {
@@ -89,6 +108,7 @@ var Game = Class.extend({
             }
         }
     },
+    // Allows callback to be called on all enemies at once
     forEachEnemy: function(callback) {
         for (var enemyID in this.enemies) {
             if (this.enemies.hasOwnProperty(enemyID)) {
@@ -98,6 +118,7 @@ var Game = Class.extend({
             }
         }
     },
+    // Allows callback to be called on all bullets at once
     forEachBullet: function(callback) {
         for (var bulletID in this.bullets) {
             if (this.bullets.hasOwnProperty(bulletID)) {
@@ -107,6 +128,7 @@ var Game = Class.extend({
             }
         }
     },
+    // Begin the game
     start: function() {
         var lastFrame = Date.now(), // Initialize the game loop
             count = 0, // Initialize the interval counter
@@ -115,23 +137,28 @@ var Game = Class.extend({
         // Update the physics of the game
 
         function physUpdate(timeElapsed) {
+            // Update each player
             self.forEachPlayer(function(player, id) {
                 player.update(timeElapsed);
             });
 
+            // Update each enemy
             self.forEachEnemy(function(enemy, id) {
                 enemy.update(timeElapsed);
             });
 
+            // Update each bullet
             self.forEachBullet(function(bullet, id) {
                 bullet.update(timeElapsed);
             });
 
+            // Spawn all of the enemies with 500ms spawn intervals
             if (self.spawningEnemies > 0) {
                 if (Date.now() - self.spawnTime > 500) {
                     self.spawnEnemy();
                 }
             } else {
+                // If all enemies are gone, start the next level
                 if (!Object.keys(self.enemies).length) {
                     self.nextLevel();
                 }
@@ -139,7 +166,6 @@ var Game = Class.extend({
         }
 
         // Serve the updated game to the clients
-
         function serveUpdate() {
             // The object containing the information the clients use to update
             var update = {
@@ -192,4 +218,5 @@ var Game = Class.extend({
     }
 });
 
+// Allow other modules to access the Game class
 module.exports = Game;
