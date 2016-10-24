3587834c69f117fb8ba1b1a8affa4e963ca98500
Tal Ben-Ari <tbenari@ucsd.edu>
2016-10-23 19:54:57 -0700

    Fixed all eslint errors in app.js

--- app.js
+++ app.js
@@ -5,132 +5,136 @@
 */
 
 // Include all of the required node modules
-var http = require('http'),
-    path = require('path'),
-    express = require('express'),
-    // Create a working node app
-    app = express(),
-    server = http.createServer(app),
-    // Instantiate a websocket server
-    io = require('socket.io').listen(server),
-    // Link all of our files together
-    router = require('./server/router'),
-    // Instantiate the lobby object at the very beginning to globalize
-    lobbies = {};
+let http = require('http')
+let path = require('path')
+let express = require('express')
+
+// Create a working node app
+let app = express()
+let server = http.createServer(app)
+
+// Instantiate a websocket server
+let io = require('socket.io').listen(server)
+
+// Link all of our files together
+let router = require('./server/router')
+
+// Instantiate the lobby object at the very beginning to globalize
+let lobbies = {}
 
 // Set the main directory for client files to the /client directory
-app.set("view options", {
-    layout: false
-});
-app.use(express.static(path.resolve(__dirname, 'client')));
-app.get('/changelog', function(req, res) {
-    res.sendfile(__dirname + '/client/changelog.html');
-});
+app.set('view options', {
+  layout: false
+})
+app.use(express.static(path.resolve(__dirname, 'client')))
+app.get('/changelog', (req, res) => {
+  res.sendfile(path.resolve('./client/changelog.html'))
+})
 
 // Generates and returns a list of all of the lobbies on the server
-function createLobbyList() {
-    var lobbyList = {};
+let createLobbyList = () => {
+  let lobbyList = {}
 
-    for (var lobby in lobbies) {
-        if (lobbies.hasOwnProperty(lobby)) {
-            lobbyList[lobby] = {
-                'playerCount': Object.keys(lobbies[lobby].clients).length
-            }
-        }
+  for (let lobby in lobbies) {
+    if (lobbies.hasOwnProperty(lobby)) {
+      lobbyList[lobby] = {
+        'playerCount': Object.keys(lobbies[lobby].clients).length
+      }
     }
+  }
 
-    return lobbyList;
+  return lobbyList
 }
 
 // Runs 'callback' on all connected clients
-function forEachSocket(callback) {
-    var clients = io.sockets.connected;
-    var clientIds = Object.keys(clients);
+let forEachSocket = callback => {
+  let clients = io.sockets.connected
+  let clientIds = Object.keys(clients)
 
-    for (var i = clientIds.length - 1; i >= 0; i--) {
-        callback(clients[clientIds[i]]);
-    }
+  for (let i = clientIds.length - 1; i >= 0; i--) {
+    callback(clients[clientIds[i]])
+  }
 }
 
 // Define what happens when a user connects to the server
-io.on('connection', function(socket) {
-    // Log the new socket's ID to the server's console
-    console.log('Socket connected:', socket.id);
+io.on('connection', socket => {
+  // Log the new socket's ID to the server's console
+  console.log('Socket connected:', socket.id)
 
-    // When signing in, send them all the lobbies
-    socket.on('signIn', function(data) {
-        socket.name = data.name;
-        socket.isInLobby = true;
-        socket.emit('lobbyList', createLobbyList());
-    });
+  // When signing in, send them all the lobbies
+  socket.on('signIn', data => {
+    socket.name = data.name
+    socket.isInLobby = true
+    socket.emit('lobbyList', createLobbyList())
+  })
 
-    // When the player requests that a new lobby be created, create a new lobby
-    socket.on('newLobby', function() {
-        var lobby = router.createLobby(5),
-            listOfLobbies;
+  // When the player requests that a new lobby be created, create a new lobby
+  socket.on('newLobby', () => {
+    let lobby = router.createLobby(5)
+    let listOfLobbies
 
-        lobbies[lobby.id] = lobby;
+    lobbies[lobby.id] = lobby
 
-        listOfLobbies = createLobbyList();
-        forEachSocket(function(socket) {
-            if (socket.isInLobby) {
-                socket.emit('lobbyList', listOfLobbies);
-            }
-        });
-    });
+    listOfLobbies = createLobbyList()
+    forEachSocket(socket => {
+      if (socket.isInLobby) {
+        socket.emit('lobbyList', listOfLobbies)
+      }
+    })
+  })
 
-    // When the player is ready to play, add them to an open lobby
-    socket.on('play', function(data) {
-        router.findLobby(lobbies, data.lobbyId).addPlayer(socket);
-        socket.isInLobby = false;
+  // When the player is ready to play, add them to an open lobby
+  socket.on('play', data => {
+    router.findLobby(lobbies, data.lobbyId).addPlayer(socket)
+    socket.isInLobby = false
 
-        var listOfLobbies = createLobbyList();
-        forEachSocket(function(socket) {
-            if (socket.isInLobby) {
-                socket.emit('lobbyList', listOfLobbies);
-            }
-        });
-    });
+    let listOfLobbies = createLobbyList()
+    forEachSocket(socket => {
+      if (socket.isInLobby) {
+        socket.emit('lobbyList', listOfLobbies)
+      }
+    })
+  })
 
-    // Process data received from the socket
-    socket.on('message', function(message) {
-        if (!socket.player) {
-            router.findOpenLobby(lobbies).addPlayer(socket);
+  // Process data received from the socket
+  socket.on('message', message => {
+    if (!socket.player) {
+      router.findOpenLobby(lobbies).addPlayer(socket)
+    }
+
+    // Do whatever the message requires
+    socket.player.parseMessage(message)
+  })
+
+  // Define what happens when a user disconnects
+  socket.on('disconnect', () => {
+    // Log the socket's disconnection w/ ID to the console
+    console.log('Socket disconnected:', socket.id)
+    if (socket.player) {
+      let listOfLobbies
+
+      // Remove the player & user from the lobby
+      console.log('Player left lobby', socket.player.lobby.id)
+      socket.player.lobby.removePlayer(socket)
+
+      // If the user was the last one in the lobby, destroy the lobby
+      if (!Object.keys(socket.player.lobby.clients).length) {
+        socket.player.lobby.remove(lobbies)
+      }
+
+      listOfLobbies = createLobbyList()
+      forEachSocket(socket => {
+        if (socket.isInLobby) {
+          socket.emit('lobbyList', listOfLobbies)
         }
-
-        // Do whatever the message requires
-        socket.player.parseMessage(message);
-    });
-
-    // Define what happens when a user disconnects
-    socket.on('disconnect', function() {
-        // Log the socket's disconnection w/ ID to the console
-        console.log('Socket disconnected:', socket.id);
-        if (socket.player) {
-            var listOfLobbies;
-
-            // Remove the player & user from the lobby
-            console.log('Player left lobby', socket.player.lobby.id);
-            socket.player.lobby.removePlayer(socket);
-
-            // If the user was the last one in the lobby, destroy the lobby
-            if (!Object.keys(socket.player.lobby.clients).length) {
-                socket.player.lobby.remove(lobbies);
-            }
-
-            listOfLobbies = createLobbyList();
-            forEachSocket(function(socket) {
-                if (socket.isInLobby) {
-                    socket.emit('lobbyList', listOfLobbies);
-                }
-            });
-        }
-    });
-});
+      })
+    }
+  })
+})
 
 // Set the server to listen for clients on localhost:3000
-server.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', function() {
-    var addr = server.address();
-    // Log the server's address to the console
-    console.log('Server listening at', addr.address + ':' + addr.port);
-});
+server.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', function () {
+  let addr = server.address()
+  // Log the server's address to the console
+  console.log('Server listening at', addr.address + ':' + addr.port)
+})
